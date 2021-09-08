b7b45feb4b32c456559cc22d835d01f769cfe7ff
Dimje <mark759@naver.com>
2021-09-08 18:29:10 +0900

    yacht

--- app/src/main/java/org/techtown/yachu/Ranking.kt
+++ app/src/main/java/org/techtown/yachu/Ranking.kt
@@ -3,11 +3,33 @@ package org.techtown.yachu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.techtown.yachu.DataBase.User
+import org.techtown.yachu.DataBase.UserDatabase
+import org.techtown.yachu.RecyclerView.Adapter
 
 class Ranking : AppCompatActivity() {
+    private lateinit var db:UserDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
+        db = UserDatabase.getInstance(applicationContext)!!
+        var users = listOf<User>()
+        CoroutineScope(Dispatchers.Main).launch {
+            users = CoroutineScope(Dispatchers.IO).async {
+                db.userDao().getAll()
+            }.await()
+        }
+        val re = findViewById<RecyclerView>(R.id.re)
+        re.layoutManager = LinearLayoutManager(this)
+
+        val adapter : Adapter = Adapter(users)
+        re.adapter = adapter
     }
 }
--- app/src/main/java/org/techtown/yachu/RecyclerView/Adapter.kt
+++ app/src/main/java/org/techtown/yachu/RecyclerView/Adapter.kt
@@ -1,11 +1,35 @@
 package org.techtown.yachu.RecyclerView
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.techtown.yachu.DataBase.User
+import org.techtown.yachu.R
 
-class Adapter(list:List<User>) : RecyclerView.Adapter<Adapter.Holder>() {
+class Adapter(val list:List<User>) : RecyclerView.Adapter<Adapter.Holder>() {
 
-    inner class Holder : RecyclerView.ViewHolder() {
 
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Adapter.Holder, position: Int) {
+        holder.name.text = list.get(position).name
+        holder.score.text = list.get(position).score.toString()
+        holder.date.text = list.get(position).date
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val name = itemView.findViewById<TextView>(R.id.name)
+        val score = itemView.findViewById<TextView>(R.id.score)
+        val date = itemView.findViewById<TextView>(R.id.date)
     }
 }
